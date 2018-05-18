6dcb84c0dfe4cb31c4ba362b4794c3a6e01565e8
Dragos Dehelean <dragos.dehelean@selenis.ro>
2018-05-18 14:59:38 +0300

    adds comments

--- modul6.5/main.js
+++ modul6.5/main.js
@@ -1,54 +1,42 @@
+// obtinem referinte la elementele HTML de care avem nevoie  
 const word = document.querySelector("#word");
-
 const button = document.querySelector("button");
+const select = document.querySelector("#s1");
 
-word.addEventListener("focus", changeBackround)
+/* punctul 1) Apelarea unei functii custom */
+word.addEventListener("focus", changeBackround);
 
 function changeBackround(){
-
     this.style.backgroundColor = "yellow";
-
 }
 
+/* punctul 2) Apelarea unei functii anonime */
 word.addEventListener("focusout", function(){
-
     this.value = this.value.toUpperCase();
-
 })
 
+/* punctul 3) Transmiterea unor parametrii catre event handler */
 button.addEventListener("click", function(){
-
     let color = word.value;
-
     changeColor(color, this);
-
-})
+});
 
 function changeColor(color, element){
-
     element.style.color = color;
-
 }
 
+/* punctul 4) Apelarea unei Closure intoarsa de o Function Factory / Emularea unei variabile statice */
 button.addEventListener("click", makeNumber(word));
-
 function makeNumber(element){
-
-    let i = 0;
-
-    return function(){
-        
-        element.value += i;
-        i++; 
-
-    }
-
+  let i = 0;
+  return function(){
+    element.value += i;
+    i++;   
+  }
 }
 
-document.querySelector("#s1").addEventListener("change", function () { 
+/* punctul 5) One more time! */
+select.addEventListener("change", function () { 
     let color = this.options[this.selectedIndex].value;
-    button.style.color = color;	
+    button.style.color = color; 
 })
-
-
-
