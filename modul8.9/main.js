38772a6fd163c19edd40af8650059b678bc7a49e
Dragos Dehelean <34175492+dragosdehelean@users.noreply.github.com>
2019-01-28 19:47:16 +0200

    dog search

--- modul8.9/main.js
+++ modul8.9/main.js
@@ -2,19 +2,19 @@
 // fetch('https://dog.ceo/api/breeds/list/all')
 //   .then(res => res.json())
 //   .then(data => {
-//     console.log(data);  
+//     console.log(data);
 //     formeazaLista(data.message);
 //   });
 
 // function formeazaLista(arr){
 //   var newArr = [];
 //   for(var breed in arr){
-    
+
 //     if(arr[breed].length === 0){
-//       newArr.push(breed); 
+//       newArr.push(breed);
 //     } else {
 //       arr[breed].forEach(item => newArr.push(breed + "-" + item) )
-      
+
 //       //console.log()
 //     }
 //   }
@@ -24,7 +24,6 @@
 //   document.write(str);
 
 // }
-
 
 /*
 In HTML:
@@ -42,105 +41,96 @@ In HTML:
 */
 
 var timeout = null;
-var showPlace = document.getElementById('dogs');
+var showPlace = document.getElementById("dogs");
 
 //PAS 1: preia inputul de la user cu un event listener
-// in event handler apeleaza functia de la Pasul 2 
+// in event handler apeleaza functia de la Pasul 2
 var search = document.getElementById("search");
 
-search.addEventListener('keyup', (e)=>{
-    
-    //ne asiguram ca stergem timeout-ul daca el exsita, ca sa nu avem intreruperi
-    // if (timeout !== null){ 
-    //     clearTimeout(timeout);
-    // }
+search.addEventListener("keyup", e => {
+  //ne asiguram ca stergem timeout-ul daca el exsita, ca sa nu avem intreruperi
+  // if (timeout !== null){
+  //     clearTimeout(timeout);
+  // }
 
-    var termen = e.target.value;
-    
+  var termen = e.target.value;
+
   //  console.log(termen);
 
-    if (termen.length > 1){
-      // Make a new timeout set to go off in 100ms
-      // timeout = setTimeout( ()=> {getSelectedBreeds(termen)}, 100);
-      
-      getSelectedBreeds(termen);     
-    }
-   
+  if (termen.length > 1) {
+    // Make a new timeout set to go off in 100ms
+    // timeout = setTimeout( ()=> {getSelectedBreeds(termen)}, 100);
+
+    getSelectedBreeds(termen);
+  }
 });
 
-//PAS 2: creeaza o functie care verifica daca string-ul se gaseste oriunde in array-ul de breeds 
+//PAS 2: creeaza o functie care verifica daca string-ul se gaseste oriunde in array-ul de breeds
 // obtine un array, selectedBreeds, cu rasele gasite
-function getSelectedBreeds(termen){
-   const selectedBreeds = breeds.filter(item => item.toLowerCase().indexOf(termen.toLowerCase()) !== -1 );
-   makeDogPromises(selectedBreeds);
+function getSelectedBreeds(termen) {
+  const selectedBreeds = breeds.filter(item => item.toLowerCase().indexOf(termen.toLowerCase()) !== -1);
+  makeDogPromises(selectedBreeds);
 }
 
-//PAS 3: - creeaza o functie care sa preia selectedBreeds (un array cu rasele corespunzatoare stringului de search), 
+//PAS 3: - creeaza o functie care sa preia selectedBreeds (un array cu rasele corespunzatoare stringului de search),
 // - formeaza endpoint-urile pentru interogarea API-ului
 // - ruleazea fetch si apoi parseze raspunsul json, pentru fiecare element din selectedBreeds
 // - pune toate Promise-urile obtinute astfel intr-un array dogPromises
-// - prinde toate Promise-urile rezolvate din array cu Promise.all().then(values => ) 
-// - apeleaza o functie concatResultLists(values) care sa concateneze array-urile de url-uri de imagini 
-//   (array-uri obtinute cu values[i].message) 
+// - prinde toate Promise-urile rezolvate din array cu Promise.all().then(values => )
+// - apeleaza o functie concatResultLists(values) care sa concateneze array-urile de url-uri de imagini
+//   (array-uri obtinute cu values[i].message)
 // - cu array-ul complet obtinut, apeleaza o functie care obtine n elemente random din el
 // - cu array-ul final apeleaza o functie care sa afiseze in pagina imaginile
-function makeDogPromises(selectedBreeds){
+function makeDogPromises(selectedBreeds) {
+  const dogPromises = [];
+  selectedBreeds.forEach(element => {
+    let promise = fetch("https://dog.ceo/api/breed/" + element + "/images").then(res => res.json());
+    dogPromises.push(promise);
+  });
 
-    const dogPromises = [];
-    selectedBreeds.forEach(element => {
-        let promise = fetch('https://dog.ceo/api/breed/' + element + '/images')
-                        .then(res => res.json());
-        dogPromises.push(promise);        
-    });
-
-    Promise.all(dogPromises)
-        .then(values => {
-            var finalArr = concatResultLists(values);
-            finalArr = randomElements(finalArr, 8);
-            printImages(finalArr);
-        });
-        
+  Promise.all(dogPromises).then(values => {
+    var finalArr = concatResultLists(values);
+    finalArr = randomElements(finalArr, 8);
+    printImages(finalArr);
+  });
 }
 
 // PAS 4: functia care concateneaza toate array-urile de tip values[i].message
-function concatResultLists(values){
+function concatResultLists(values) {
+  var finalArr = [];
 
-   var finalArr = [];
+  values.forEach(element => {
+    //element.message
+    finalArr = finalArr.concat(element.message);
+  });
 
-   values.forEach(element => {
-      //element.message       
-      finalArr = finalArr.concat(element.message);
-   });   
-
-   return finalArr;
+  return finalArr;
 }
 
 // PAS 5: functia care obtine n elemente random dintr-un array trimis ca parametru
-function randomElements(arr1, n){
-
-    var imageArr = []; 
-    for(var i=0; i < n; i++){
-      let randElem = arr1[Math.floor(Math.random() * arr1.length)];
-      if ( randElem !== null && typeof randElem !=='undefined' ){  
-         imageArr.push(randElem);
-      }
+function randomElements(arr1, n) {
+  var imageArr = [];
+  for (var i = 0; i < n; i++) {
+    let randElem = arr1[Math.floor(Math.random() * arr1.length)];
+    if (randElem !== null && typeof randElem !== "undefined") {
+      imageArr.push(randElem);
     }
+  }
 
-    return imageArr;
+  return imageArr;
 }
 
 // PAS 6: functia care printeaza in HTML array-ul cu imaginile finale
 // PAS 7 (optional): afisati sub fiecare imagine si rasa din care face parte
 // (nota: e nevoie de un Regex putin mai complicat)
-function printImages(arr2){   
-    var html = '';
-    arr2.forEach(item => {
-      let reg = /[\w-]+(?=\/n)/g;
-      let rasa = item.match(reg); 
-      html += '<div class="col-4"><img src="' + item +'" class="img-fluid">';
-      html += '<br>' + (rasa ? rasa : rasa[0]) +'</div>';      
+function printImages(arr2) {
+  var html = "";
+  arr2.forEach(item => {
+    let reg = /[\w-]+(?=\/n)/g;
+    let rasa = item.match(reg);
+    html += '<div class="col-4"><img src="' + item + '" class="img-fluid">';
+    html += "<br>" + (rasa ? rasa : rasa[0]) + "</div>";
+  });
 
-    });
-
-    showPlace.innerHTML = html;
+  showPlace.innerHTML = html;
 }
